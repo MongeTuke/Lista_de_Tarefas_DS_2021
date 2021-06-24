904779b75bdfcd0208410680aae0e7a5290d3efd
Tulio Henrique <tulioh388@gmail.com>
2021-06-24 09:47:37 -0300

    atualizando o codigo

--- arquivo.js
+++ arquivo.js
@@ -1,37 +1,37 @@
 const novaTarefa = document.querySelector('[data-form-button]');
-const inputTarefa = document.querySelector('[data-form-input]');
 
-function criarBotaoDelete(){
+function criarBotaoDelete() {
     const botaoDelete = document.createElement('span');
     botaoDelete.innerText = "x";
     botaoDelete.classList = "close";
     botaoDelete.addEventListener('click', deletarTarefa);
     return botaoDelete;
 }
- 
-function deletarTarefa(evento){
+
+function deletarTarefa(evento) {
     const botaoDeleteClicado = evento.target
     const itemDaLista = botaoDeleteClicado.parentElement
     itemDaLista.remove()
 }
 
-function criarBotaoConcluir(){
+function criarBotaoConcluir() {
     const botaoConcluir = document.createElement('input')
     botaoConcluir.setAttribute('type', 'checkbox')
     botaoConcluir.classList = "form-check-input"
     botaoConcluir.addEventListener('click', concluirTarefa)
 
     return botaoConcluir
-} 
+}
 
-function concluirTarefa(evento){
+function concluirTarefa(evento) {
     const botaoConcluirClicado = evento.target
     const itemDaListaConcluido = botaoConcluirClicado.parentElement
     itemDaListaConcluido.classList.toggle('tarefa_concluida')
 }
 
-function criarTarefa(evento){
+function criarTarefa(evento) {
     evento.preventDefault()
+    const inputTarefa = document.querySelector('[data-form-input]');
     const valorTarefa = "- " + inputTarefa.value + '.'
     const listaDeTarefas = document.querySelector('[data-task]')
 
